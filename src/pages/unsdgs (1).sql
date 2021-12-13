5bace6c3aa8a263d79cee0326030a1b3045c6111
ART <pakkawat.kh@gmail.com>
2021-12-13 12:34:03 +0700

    add database

---
+++ src/pages/unsdgs (1).sql
@@ -0,0 +1,148 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 13, 2021 at 05:33 AM
+-- Server version: 8.0.17
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `unsdgs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `adminmode` tinyint(1) NOT NULL,
+  `token` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `adminmode`, `token`) VALUES
+(1, 'admin', '4444', 1, 'i0hdeFxUOa'),
+(4, 'test', '1234', 0, 'aQpkDYsqoh');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL,
+  `orderid` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `answer1` text NOT NULL,
+  `answer2` text NOT NULL,
+  `answer3` text NOT NULL,
+  `answer4` text NOT NULL,
+  `correctAnswer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`id`, `orderid`, `question`, `answer1`, `answer2`, `answer3`, `answer4`, `correctAnswer`) VALUES
+(1, 100, 'What does SDGs stand for?', 'Standard development goals', 'Sustainable development goals', 'Standard deployment goals', 'Sustainable deployment goals', 2),
+(2, 200, 'What does SDGs stand forxx?', 'Standard development goalsxx', 'Sustainable development goalsxx', 'Standard deployment goalsxx', 'Sustainable deployment goalsxxx', 3),
+(3, 300, 'test1', 'xxx', 'yyyz', 'zzzx', 'aaa', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `email` text NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `gender` text NOT NULL,
+  `country` text NOT NULL,
+  `jobtitle` text NOT NULL,
+  `organization` text NOT NULL,
+  `score` int(11) NOT NULL DEFAULT '0',
+  `complete` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `firstname`, `lastname`, `gender`, `country`, `jobtitle`, `organization`, `score`, `complete`) VALUES
+(1, 'Artzy', 'artzy@gmail.com', 'art', 'test', 'Male', 'Thailand', 'Teacher', 'Chula', 30, 1),
+(2, 'rest', 'rest@gmail.com', 'rest', 'data', 'Female', 'Japan', 'Teacher', 'Test', 38, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
